927dd5cdbc67e4382e5caa4e8f1331a4ba67ef4b
simenhu <simen@primecut.no>
2016-09-30 11:52:36 +0200

    FIxed method call in Reciever

--- oving3/koder.py
+++ oving3/koder.py
@@ -23,11 +23,12 @@ class Cipher():
     def get_key(self):
         pass
 
-
+    def set_key(self):
+        pass
 class Caesar_Cipher(Cipher):
     def __init__(self, offset=None):
         if(offset == None):
-            self.offset = self.generate_keys()
+            self.generate_keys()
         else:
             self.offset = offset
     def encode(self, data):
@@ -35,11 +36,10 @@ class Caesar_Cipher(Cipher):
         return "".join([self.chars[(self.chars.index(x)+self.offset)%self.char_count] for x in data])
 
     def decode(self, data, key):
-            if key != None:
-                return "".join([self.chars[(self.chars.index(x)+(self.char_count-key))%self.char_count] for x in data])
-            else:
-                return "".join(
-                    [self.chars[(self.chars.index(x) + (self.char_count - self.offset)) % self.char_count] for x in data])
+            if key == None:
+                key=self.offset
+            return "".join([self.chars[(self.chars.index(x)+(self.char_count-key))%self.char_count] for x in data])
+
     def verify(self):
         test_string = "".join([chr(rd.randint(32, 126)) for x in range(0, 10)])
         print("Test string is:%s"%test_string)
@@ -52,11 +52,13 @@ class Caesar_Cipher(Cipher):
 
     def generate_keys(self):
         self.offset = rd.randint(0,94)
-        return self.offset
+
 
     def get_key(self):
         return self.offset
 
+    def set_key(self,key):
+        self.offset = key
 
 class Multiplicative(Cipher):
     def __init__(self):
@@ -76,7 +78,7 @@ class Multiplicative(Cipher):
         decoded_list = []
         for i in data:
             index = self.chars.index(i)
-            new_index = (index * self.decode_key) % self.char_count
+            new_index = (index * key) % self.char_count
             decoded_list.append(self.chars[new_index])
         return "".join(decoded_list)
 
@@ -85,10 +87,56 @@ class Multiplicative(Cipher):
         while not gcd(n,95) == 1:
             n = rd.randint(1, 500)
         self.encode_key = n
-        self.decode_key = crypto_utils.modular_inverse(n,95)
+        m = crypto_utils.modular_inverse(n,95)
+        self.decode_key = m
 
     def get_key(self):
         return self.decode_key
+
+    def set_key(self, encode, decode):
+        self.encode_key = encode
+        self.decode_key = decode
+
+    def verify(self):
+        test_string = "".join([chr(rd.randint(32, 126)) for x in range(0, 10)])
+        print("Test string is:%s" % test_string)
+        encoded = self.encode(test_string)
+        print("Encoded string is%s" % encoded)
+        decoded = self.decode(encoded)
+        print("Decoded string is:%s" % decoded)
+        if decoded == test_string:
+            print("text was transfered sucsessfully")
+
+class Affine(Cipher):
+    def __init__(self):
+        self.mult = Multiplicative()
+        self.caecar = Caesar_Cipher()
+        self.decode_key = (self.mult.get_key(), self.caecar.get_key())
+
+    def generate_keys(self):
+        self.mult.generate_keys()
+        self.caecar.generate_keys()
+        self.decode_key =(self.mult.get_key(), self.caecar.get_key())
+
+    def set_key(self, key):
+        self.decode_key = key
+
+    def encode(self, data):
+        mult_crypt = self.mult.encode(data)
+        affine_crypt = self.caecar.encode(mult_crypt)
+        return affine_crypt
+
+
+    def decode(self,data, key = None):
+        if key == None:
+            key = self.decode_key
+        caecar_uncrypt = self.caecar.decode(key[1])
+        affine_uncrypt = self.mult.decode(caecar_uncrypt, key[0])
+        return affine_uncrypt
+
+    def get_key(self):
+        return self.decode_key
+
     def verify(self):
         test_string = "".join([chr(rd.randint(32, 126)) for x in range(0, 10)])
         print("Test string is:%s" % test_string)
@@ -131,7 +179,7 @@ class Reciever(Person):
         super(Reciever, self).__init__(cipher, key)
 
     def operate_cipher(self, data):
-        return self.cipher.decode(data, self.key)
+        return self.cipher.decode(data ,self.key)
 
 
 
@@ -141,6 +189,7 @@ class Hacker():
 def main():
 
     sender = Sender(Multiplicative())
+    print("sender key(decode key): %d"%sender.get_key())
     reciever = Reciever(Multiplicative(), sender.get_key())
     input_text = input("write your message \n>>>")
 
@@ -152,5 +201,9 @@ def main():
         input_text = input("write your message \n>>>")
     print("Goodbye, your secrets has been kept safe!")
 
+def test():
+    mult = Affine()
+    mult.verify()
+
 if __name__ == '__main__':
-    main()
+    test()
